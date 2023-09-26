9a01f420a75698c333d11815d2130563563ac6aa
user <me@local.machine>
2023-09-26 11:10:39 +0530

    half done

---
+++ src/routes/fullnews/[fullarticle]/+page.js
@@ -0,0 +1,7 @@
+// // /** @type {import('@sveltejs/kit').Load} */
+
+// @ts-ignore
+export const load = ({ params, route }) => {
+    // @ts-ignore
+    console.log(route.id);
+}
---
+++ src/routes/fullnews/[fullarticle]/+page.svelte
@@ -0,0 +1,148 @@
+<section class="text-gray-600 body-font">
+  <div class="container px-5 py-24 mx-auto flex flex-col">
+    <div class="lg:w-4/6 mx-auto">
+      <h3 class="text-5xl font-bold text-gray-800">
+        Government rebuffs Moody's Aadhaar views; says a billion Indians trust
+        it
+      </h3>
+      <h5 class="text-lg text-gray-600 font-semibold mt-6">
+        Electronics and IT Ministry asserted that there have been no breaches
+        reported from the unique ID system's database till date to dismiss
+        concerns about security and privacy in a centralised system.
+      </h5>
+
+      <p class="text-sm text-gray-400 mt-2 mb-10">
+        September 25, 2023 11:09 pm | Updated 11:48 pm IST - NEW DELHI
+      </p>
+
+      <div class="rounded-lg h-64 overflow-hidden">
+        <img
+          alt="content"
+          class="object-cover object-center h-full w-full"
+          src="https://dummyimage.com/1200x500"
+        />
+      </div>
+
+      <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8 flex justify-between">
+        <div
+          class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400"
+        >
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-10 h-10"
+            viewBox="0 0 24 24"
+          >
+            <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2" />
+            <circle cx="12" cy="7" r="4" />
+          </svg>
+        </div>
+        <div
+          class="flex flex-col items-center text-center justify-center min-w-max"
+        >
+          <h2 class="font-medium title-font mt-4 text-gray-900 text-base">
+            The Hindu Bureau
+          </h2>
+          <div class="w-full h-1 bg-indigo-500 rounded mt-2 mb-4" />
+        </div>
+      </div>
+
+      <div class="flex flex-col">
+        <!-- for each para render p tag  -->
+          <p class="leading-relaxed text-lg mb-4">
+            The Electronics and IT Ministry late on September 25 took on a
+            report from Moody’s Investors Service that raised questions about
+            the efficacy of Aadhaar and asserted that there have been no
+            breaches reported from the unique ID system’s database till date to
+            dismiss concerns about security and privacy in a centralised
+            system.In a note titled “Aadhaar, the most trusted digital ID in the
+            world — Moody’s Investors Service opinions baseless” , the Ministry
+            said a number of international agencies, including the IMF and the
+            World Bank, have lauded Aadhaar and several nations have also
+            engaged with the Unique Identification Authority of India (UIDAI) to
+            understand how they may deploy similar digital ID systems. Moreover,
+            it emphasised that over a billion Indians have also expressed their
+            trust in Aadhaar by using it to authenticate themselves “over 100
+            billion times”.
+            </p>
+            <p class="leading-relaxed text-lg mb-4">a report last Saturday, the global rating agency
+            had said that Aadhaar’s biometric authentication systems often
+            results in “service denials” for users and are unreliable in hot and
+            humid conditions. The agency had also red-flagged security and
+            privacy risks to users of such centralised systems where a single
+            entity such as a government electoral roll “controls and manages a
+            user’s identifying credentials and their access to online
+            resources”.“A certain investor service has, without citing any
+            evidence or basis, made sweeping assertions against Aadhaar, the
+            most trusted digital ID in the world,” the Electronics and IT
+            Ministry’s riposte noted. “Over the last decade, over a billion
+            Indians have expressed their trust in Aadhaar by using it to
+            authenticate themselves over 100 billion times. To ignore such an
+            unprecedented vote of confidence in an identity system is to imply
+            that the users do not understand what is in their own interest,” it
+            averred.“The report in question does not cite either primary or
+            secondary data or research in support of the opinions presented in
+            it. The investor service did not make any attempt to ascertain facts
+            regarding the issues raised by it from the Authority,” the Ministry
+            pointed out, before stressing that Aadhaar is the foundational
+            Digital Public Infrastructure (DPI) of the India stack.The recent
+            G-20 New Delhi Declaration has welcomed India’s plan to build and
+            maintain a Global Digital Public Infrastructure Repository (GDPIR),
+            a virtual DPI repository, voluntarily shared by G-20 members and
+            beyond, it added.“The report avers that use of biometric
+            technologies results in service denials for manual labourers in
+            India’s hot, humid climate, an obvious reference to India’s Mahatma
+            Gandhi National Rural Employment Guarantee Scheme [MGNREGS],” the
+            Ministry noted. “However, it is evident that the authors of the
+            report are unaware that the seeding of Aadhaar in the MGNREGS
+            database has been done without requiring the worker to authenticate
+            using their biometrics, and that even payment to workers under the
+            scheme is made by directly crediting money into their account and
+            does not require the worker to authenticate using their biometrics,”
+            it argued.The Moody’s report also ignores that biometric submission
+            is also possible through contactless means like face authentication
+            and iris authentication, the Electronics and IT Ministry said,
+            adding that the option of mobile OTP (One-Time Passcode) is also
+            available in many use cases.Rebuffing doubts about security and
+            privacy vulnerabilities in a centralised Aadhaar system, the
+            Ministry said that the facts on these aspects have been repeatedly
+            disclosed in response to Parliament questions.“Further, Parliament
+            has laid down robust privacy protections in the law governing the
+            Aadhaar system and these are observed through robust technological
+            and organisational arrangements. State-of-the-art security solutions
+            are in place, along with a federated database and encryption of data
+            both at rest and in motion,” it said. These systems are certified as
+            per international security and privacy standards (ISO 27001:2013 for
+            Information Security Management System and ISO 27701:2019 for
+            Privacy Information Management System), the Ministry emphasised.The
+            Electronics and IT Ministry also highlighted that the G-20 Global
+            Partnership for Financial Inclusion (GPFI), in a report prepared by
+            the World Bank, has stated that the “implementation of DPIs such as
+            Aadhaar (a foundational digital ID system), along with the Jan Dhan
+            bank accounts, and mobile phones, is considered to have played a
+            critical role in enhancing ownership of transaction accounts. Such
+            accounts, it said, have gone up from approximately one-fourth of
+            adults in 2008 to over 80% now — “a journey that it is estimated
+            could have taken up to 47 years without DPIs”.
+          </p>
+          <a target="_blank" href="/" class="text-indigo-500 inline-flex items-center"
+            >Read Original Article
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7" />
+            </svg>
+          </a>
+      </div>
+    </div>
+  </div>
+</section>
