dff0b71b93824b3583f90a8b7dd3b1f650ecef8c
user <me@local.machine>
2023-09-29 20:10:30 +0530

    Summarize button working

--- src/components/Article.svelte
+++ src/components/Article.svelte
@@ -5,6 +5,22 @@
      * @type {{ article: { url: string; title: string; subTitle: string; pubDate: string; image: string | undefined; author: string; parr: string[] }; }}
      */
     export let data;
+    let buttonState = "idle";
+    let summarizedArticle = "";
+
+    function summarize() {
+        buttonState = "summarizing";
+
+        // summarizedArticle = await mlModel(data.article.parr.join('') as complete string);
+        // buttonState = "summarized";
+
+        setTimeout(() => {
+            buttonState = "summarized";
+            summarizedArticle =
+                "EU industry chief Thierry Breton has urged Apple CEO Tim Cook to open Apple's ecosystem to competitors under the Digital Markets Act. Breton's request comes after theirmeeting in Brussels. He emphasized the need for iPhone users to access competitive services like electronic wallets,  browsers, and app stores. Apple has not responded. The Act aims to boost competition, and Breton challenged Apple's security and privacy arguments, stating that EU regulation can foster innovation while ensuring security and privacy.";
+        }, 3000);
+
+    }
 </script>
 
 <section class="text-gray-600 body-font">
@@ -51,7 +67,7 @@
             </div>
 
             <div
-                class="sm:w-1/3 text-center sm:pr-8 sm:py-8 flex justify-between"
+                class="text-center sm:pr-8 sm:py-8 flex space-x-3"
             >
                 <div
                     class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400"
@@ -80,16 +96,11 @@
                     <div class="w-full h-1 bg-indigo-500 rounded mt-2 mb-4" />
                 </div>
             </div>
-            <div class="hidden border-dotted border-blue-400 border-2 p-2 rounded text-justify mb-4">
-                EU industry chief Thierry Breton has urged Apple CEO Tim
-                Cook to open Apple's ecosystem to competitors under the
-                Digital Markets Act. Breton's request comes after their
-                meeting in Brussels. He emphasized the need for iPhone users
-                to access competitive services like electronic wallets,
-                browsers, and app stores. Apple has not responded. The Act
-                aims to boost competition, and Breton challenged Apple's
-                security and privacy arguments, stating that EU regulation
-                can foster innovation while ensuring security and privacy.
+            <div
+                class="{(buttonState === 'idle' || buttonState === 'summarizing') &&
+                    'hidden'} border-dotted border-blue-400 border-2 p-2 rounded text-justify mb-4"
+            >
+                {summarizedArticle}
             </div>
             <div
                 class="py-2 flex justify-between items-center border-spacing-6 border-dashed border-b-2 border-blue-500"
@@ -101,27 +112,33 @@
                     <span>&nbsp;&nbsp;Summarize the article!</span>
                 </div>
                 <button
-                    disabled
+                    disabled={buttonState === "summarizing" ||
+                        buttonState === "summarized"}
+                    on:click={() => summarize()}
                     type="button"
-                    class="text-white bg-blue-100 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+                    class="{buttonState === 'summarized' &&
+                        'opacity-50 cursor-not-allowed'} text-white bg-blue-100 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
                 >
-                    <!-- <svg
-                        aria-hidden="true"
-                        role="status"
-                        class="inline w-4 h-4 mr-3 text-white animate-spin"
-                        viewBox="0 0 100 101"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                            fill="#E5E7EB"
-                        />
-                        <path
-                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                            fill="currentColor"
-                        />
-                    </svg> -->
+                    {#if buttonState === "summarizing"}
+                        <svg
+                            aria-hidden="true"
+                            role="status"
+                            class="inline w-4 h-4 mr-3 text-white animate-spin"
+                            viewBox="0 0 100 101"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="#E5E7EB"
+                            />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentColor"
+                            />
+                        </svg>
+                    {/if}
+
                     Summarize
                 </button>
             </div>
